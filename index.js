c348ed172f34b08705fe18a9d68f22f5e4ebcb79
2G2-99 <79936466+2G2-99@users.noreply.github.com>
2025-01-30 18:34:31 +0000

    Started adding elements for the UI Added a header, three button choices

--- index.js
+++ index.js
@@ -1,146 +1,106 @@
+// # DOM
+
 /*
-Upon run of the program, a prompt will appear asking for an input of "Rock", "Paper" or "Scissors". Then the program will compare the input against a random result from the machine, and depending on the combination the user might win or lose.
-
-To win the game, the player must beat the machine in two of the three rounds.
-
-roundTarget;
-roundCounter;
-playerPoints;
-machinePoints;
-
-Input move
-  if move is "Rock"
-    store Player's move
-  if move is "Paper"
-    store Player's move
-  if move is "Scissors"
-    store Player's move
-  else
-    alert
-    input move again
-
-Generate machine move
-  generate random integer
-  if random integer is 1
-    the move is "Rock"
-  if random integer is 2
-    the move is "Paper"
-  if random integer is 3
-    the move is "Scissors"
-  store machine move
-
-Winning move
-  if "Rock" VS "Paper"
-    "Paper" wins
-  if "Paper" VS "Scissors"
-    "Scissors" wins
-  if "Scissors" VS "Rock"
-    "Rock" wins
-
-Play round
-  roundCounter + 1
-  if PlayerMove and MachineMove are the same
-    no points
-  if PlayerMove beats machineMove
-    playerPoints + 1
-  if machineMove beats PlayerMove
-    machinePoints + 1
-
-Play Game
-  while roundCounter < roundTarget
-    return playRound
-  if playerPoints > machinePoints
-    return "Player wins with ___ points"
-  else
-    return "Machine wins with ___ points"
+ * Change all console.logs for DOM elements
 
  */
 
+const playGround = document.querySelector('#playground');
+const selectionButtons = document.createElement('div');
+
+selectionButtons.setAttribute('id', 'selection');
+selectionButtons.classList.add('container');
+
+const ROCK = document.createElement('button');
+ROCK.setAttribute('id', 'rock');
+ROCK.textContent = 'ROCK';
+selectionButtons.appendChild(ROCK);
+
+const PAPER = document.createElement('button');
+PAPER.setAttribute('id', 'paper');
+PAPER.textContent = 'PAPER';
+selectionButtons.appendChild(PAPER);
+
+const SCISSORS = document.createElement('button');
+SCISSORS.setAttribute('id', 'scissors');
+SCISSORS.textContent = 'SCISSORS';
+selectionButtons.appendChild(SCISSORS);
+
+let choicesList = selectionButtons.querySelectorAll('button');
+
+choicesList.forEach(choice => {
+  choice.classList.add('btn');
+  choice.addEventListener('click', handleChoice);
+});
+
+playGround.appendChild(selectionButtons);
+playGround.appendChild(messages);
+
+//////////////////////////////////////////////////////////////////
+
+// # GAME
 let roundCounter = 1;
 let playerScore = 0;
 let computerScore = 0;
 
 function getComputerChoice() {
   let randomNumber = parseInt(Math.random() * 3);
+
   switch (randomNumber) {
     case 0:
-      window.alert(`The machine is playing with "rock"`);
-      return "rock";
+      console.log(`The machine is playing with "rock"`);
+      return 'rock';
       break;
     case 1:
-      window.alert(`The machine is playing with "paper"`);
-      return "paper";
+      console.log(`The machine is playing with "paper"`);
+      return 'paper';
       break;
     case 2:
-      window.alert(`The machine is playing with "scissors"`);
-      return "scissors";
+      console.log(`The machine is playing with "scissors"`);
+      return 'scissors';
   }
 }
 
-function getPlayerChoice() {
-  let input = window.window
-    .prompt(
-      'Please select one of the three movements.\n"Rock", "Paper" or "Scissors"',
-      ""
-    )
-    .toLowerCase();
+function getPlayerChoice(e) {
+  let choice = e.target;
 
-  if (input === "rock" || input === "paper" || input === "scissors") {
-    window.alert(`You are playing with "${input}"`);
-    return input;
-  } else {
-    window.alert("Please enter a valid move");
-    return getPlayerChoice();
+  if (
+    choice.id === 'rock' ||
+    choice.id === 'paper' ||
+    choice.id === 'scissors'
+  ) {
+    console.log(`You are playing with "${choice.id}"`);
+    choice.setAttribute('style', 'background: #1E1E24; color: #F7F7FF; ');
+    return choice.id;
   }
+}
+
+function handleChoice(e) {
+  const playerChoice = getPlayerChoice(e);
+  const computerChoice = getComputerChoice();
+
+  playRound(playerChoice, computerChoice);
 }
 
 function playRound(playerChoice, computerChoice) {
   if (playerChoice === computerChoice) {
-    window.alert(`It's a tie, no one scores!.`);
-    window.alert(
-      `The score is:\n Player ${playerScore} - CPU ${computerScore}`
-    );
+    console.log(`It's a tie, no one scores!.`);
+    console.log(`The score is:\n Player ${playerScore} - CPU ${computerScore}`);
   } else if (
-    (playerChoice === "rock" && computerChoice === "scissors") ||
-    (playerChoice === "paper" && computerChoice === "rock") ||
-    (playerChoice === "scissors" && computerChoice === "paper")
+    (playerChoice === 'rock' && computerChoice === 'scissors') ||
+    (playerChoice === 'paper' && computerChoice === 'rock') ||
+    (playerChoice === 'scissors' && computerChoice === 'paper')
   ) {
     playerScore += 1;
-    window.alert(`You won, ${playerChoice} beats ${computerChoice}!`);
-    window.alert(
-      `The score is:\n Player ${playerScore} - CPU ${computerScore}`
-    );
+    console.log(`You won, ${playerChoice} beats ${computerChoice}!`);
+    console.log(`The score is:\n Player ${playerScore} - CPU ${computerScore}`);
   } else if (
-    (computerChoice === "rock" && playerChoice === "scissors") ||
-    (computerChoice === "paper" && playerChoice === "rock") ||
-    (computerChoice === "scissors" && playerChoice === "paper")
+    (computerChoice === 'rock' && playerChoice === 'scissors') ||
+    (computerChoice === 'paper' && playerChoice === 'rock') ||
+    (computerChoice === 'scissors' && playerChoice === 'paper')
   ) {
     computerScore += 1;
-    window.alert(`You lose, ${computerChoice} beats ${playerChoice}!`);
-    window.alert(
-      `The score is:\n Player ${playerScore} - CPU ${computerScore}`
-    );
+    console.log(`You lose, ${computerChoice} beats ${playerChoice}!`);
+    console.log(`The score is:\n Player ${playerScore} - CPU ${computerScore}`);
   }
 }
-
-function playGame() {
-  while (roundCounter <= 5) {
-    window.alert(`ROUND ${roundCounter}`);
-    playerMove = getPlayerChoice();
-    computerMove = getComputerChoice();
-    playRound(playerMove, computerMove);
-    roundCounter += 1;
-  }
-
-  window.alert(`FINAL SCORE:\n Player ${playerScore} - CPU ${computerScore}`);
-
-  if (playerScore > computerScore) {
-    window.alert(`You win the game!`);
-  } else if (computerScore > playerScore) {
-    window.alert(`You lose the game!`);
-  } else {
-    window.alert(`Its a draw`);
-  }
-}
-
-playGame();
